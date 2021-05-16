4a1e5288e56e47d7a0b39964df294ea4f6ae3c2f
poornesh-tata <80261388+poornesh-tata@users.noreply.github.com>
2021-05-16 20:44:17 +0530

    Update [Switch]_For_operations_on_arrays.c

--- [Switch]_For_operations_on_arrays.c
+++ [Switch]_For_operations_on_arrays.c
@@ -80,7 +80,7 @@ void main()
   int i,j,temp;    
  for(i=0;i<n-1;i++)
   {
-   for(j=0;j<n-1;j++)
+   for(j=0;j<n-i-1;j++)
      {
        if(arr[j]>arr[j+1])
        {
@@ -102,7 +102,7 @@ void main()
   int i,j,temp;
  for(i=0;i<n-1;i++)
   {
-   for(j=0;j<n-1;j++)
+   for(j=0;j<n-i-1;j++)
      {
        if(arr[j]<arr[j+1])
        {
